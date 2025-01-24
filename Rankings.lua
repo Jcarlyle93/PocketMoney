21cb138cc6f2a6c14742bae456f8ef4747df3493
jcarlyle93 <129001436+Jcarlyle93@users.noreply.github.com>
2025-01-24 21:43:03 +0000

    Minor Bugfix

--- Rankings.lua
+++ Rankings.lua
@@ -1,7 +1,7 @@
 local ADDON_PREFIX = "PMRank"
+PocketMoneyRankings = PocketMoneyRankings or {}
 local CHANNEL_NAME = "PCMSync"
 local CHANNEL_PASSWORD = "pm" .. GetRealmName()
-PocketMoneyRankings = PocketMoneyRankings or {}
 
 local function RegisterAddonPrefix()
   if not C_ChatInfo.IsAddonMessagePrefixRegistered(ADDON_PREFIX) then
